ddb8c664d522bef05c9f9571930244696a0bd7ca
Timmy O'Mahony <hey@timmyomahony.com>
2023-11-12 20:05:33 +0000

    Added permalink to image

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,12 +1,37 @@
 "use client";
+
+import Link from "next/link";
 import { useState } from "react";
-import Lightbox from "yet-another-react-lightbox";
+import {
+  Lightbox,
+  IconButton,
+  useLightboxState,
+} from "yet-another-react-lightbox";
+import { useRouter } from "next/navigation";
 import Captions from "yet-another-react-lightbox/plugins/captions";
 import MasonryGrid from "@/components/MasonryGrid";
 import CustomSliderImage from "@/components/CustomSliderImage";
 import type { Photo } from "@/types/album";
 import LeftArrowIcon from "@/icons/LeftArrow.svg";
 import CloseIcon from "@/icons/Close.svg";
+import ExternalLinkIcon from "@/icons/ExternalLink.svg";
+
+const ExternalLinkButton = () => {
+  const router = useRouter();
+  const { currentSlide } = useLightboxState();
+  return (
+    <IconButton
+      onClick={() => {
+        if (currentSlide) {
+          router.push(`/photo/${currentSlide.uuid}/`);
+        }
+      }}
+      label="View photo"
+      icon={ExternalLinkIcon}
+      disabled={!currentSlide}
+    />
+  );
+};
 
 const Gallery = ({ photos }: { photos: Photo[] }) => {
   const [open, setOpen] = useState<boolean>(false);
@@ -29,17 +54,13 @@ const Gallery = ({ photos }: { photos: Photo[] }) => {
         open={open}
         plugins={[Captions]}
         toolbar={{
-          buttons: [
-            <button key="my-button" type="button" className="yarl__button">
-              Button
-            </button>,
-            "close",
-          ],
+          buttons: [<ExternalLinkButton key="external-link-button" />, "close"],
         }}
         close={() => setOpen(false)}
         slides={photos.map((photo) => {
           return {
             src: photo.url,
+            uuid: photo.uuid,
             height: photo.height,
             width: photo.width,
             title: photo.exif.title,
@@ -49,9 +70,15 @@ const Gallery = ({ photos }: { photos: Photo[] }) => {
         })}
         render={{
           slide: CustomSliderImage,
-          iconPrev: () => <LeftArrowIcon className="yarl__icon transition-all duration-200 ease-linear hover:-translate-y-1" />,
-          iconNext: () => <LeftArrowIcon className="yarl__icon rotate-180 transition-all duration-200 ease-linear hover:-translate-y-1" />,
-          iconClose: () => <CloseIcon className="yarl__icon w-6" />
+          iconPrev: () => (
+            <LeftArrowIcon className="yarl__icon transition-all duration-200 ease-linear hover:-translate-y-1" />
+          ),
+          iconNext: () => (
+            <LeftArrowIcon className="yarl__icon rotate-180 transition-all duration-200 ease-linear hover:-translate-y-1" />
+          ),
+          iconClose: () => (
+            <CloseIcon className="yarl__icon w-5 transition-all duration-200 ease-linear hover:-translate-y-1" />
+          ),
         }}
       />
     </section>
