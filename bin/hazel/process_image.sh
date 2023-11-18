70856644c8f2a8a93ea4ba8211784b32d4ae192c
Timmy O'Mahony <hey@timmyomahony.com>
2023-11-18 16:43:50 +0000

    Updated the readme

--- bin/hazel/process_image.sh
+++ bin/hazel/process_image.sh
@@ -1,17 +1,15 @@
 #!/bin/zsh
 
-# Process image
+# Process Image
 #
 # This script is launched via Hazel, where $1 is the path of a new image
-# exported via Lightroom.
+# exported via Lightroom to a monitored folder.
 #
 # The containing folder for the image should be the album and its date, in the
 # format "2023-01-01 Misty Morning in Glasgow"
 #
-# These jpegs should be at 100% quality and dimensions.
-#
-# The script will scale the images down then upload them to a folder in
-# Digital Ocean.
+# These jpegs should be at 100% quality and dimensions. The script will scale
+# the images down then upload them to a folder in Digital Ocean.
 #
 # Dependencies
 # ============
@@ -23,6 +21,7 @@
 #
 # https://imagemagick.org/script/mogrify.php
 # https://www.noodlesoft.com/manual/hazel/attributes-actions/using-shell-scripts/
+# http://www.imagemagick.org/script/command-line-processing.php#geometry
 
 IMAGE_PATH=$1
 IMAGE_FILENAME="${IMAGE_PATH##*/}"
@@ -48,13 +47,11 @@ if [[ $IMAGE_PATH == *"Thumbnails"*  ]]; then
     return 0;
 fi
 
-# Create thumbnails folder
 if [ ! -d "$THUMBNAIL_DIR" ]; then
     mkdir -p "$THUMBNAIL_DIR"
 fi
 
-# Reduce quality of images for web
-# http://www.imagemagick.org/script/command-line-processing.php#geometry
+# Prepare images for the web
 mogrify -quality 95 -resize "2048x2048" "$IMAGE_PATH"
 
 # Create social media thumbnail
@@ -62,6 +59,10 @@ convert "$IMAGE_PATH" -quality 100 -resize "1200x630^" -gravity "center" -extent
 mogrify -bordercolor white -border "20x20" "$THUMBNAIL_PATH"
 exiftool -TagsFromFile "$IMAGE_PATH" -all:all -overwrite_original "$THUMBNAIL_PATH"
 
-# Upload to Digital Ocean
+# Upload files
+#
+# NOTE: This command is using a local profile configured in ~/.aws/credentials
+# called "personal". You need to either create this profile manually or remove
+# the `--profile` flag altogether.
 aws s3 cp "$IMAGE_PATH" "s3://$AWS_BUCKET/$RELATIVE_IMAGE_PATH" --endpoint-url "$AWS_ENDPOINT" --acl public-read --profile personal
 aws s3 cp "$THUMBNAIL_PATH" "s3://$AWS_BUCKET/$RELATIVE_THUMBNAIL_PATH" --endpoint-url "$AWS_ENDPOINT" --acl public-read --profile personal
